2770064e696d60e40c66cd75c2dbc84c8e6eb37b
Li Jiaxin <lijiax50@teach.cs.toronto.edu>
2025-06-12 15:23:57 -0400

    Setup environment on CS Lab

--- pyhanabi/submit_job.sh
+++ pyhanabi/submit_job.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --partition=long
-#SBATCH --cpus-per-task=4
-#SBATCH --gres=gpu:rtx8000:2
-#SBATCH --mem=48G
-#SBATCH --time=71:59:00
-#SBATCH -o ${SCRATCH}/llm_hanabi_hive/slurm/cool_job-%j.out
+#SBATCH --partition=csc494-proj
+#SBATCH --gres=gpu:2
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=96G 
+#SBATCH --time=4:00:00
+#SBATCH --output=logs/r3d2_%j.out
 
 
 FREQ=$1
@@ -14,9 +14,9 @@ PLAYER=$4
 ADDLAYER=$5
 WANDB="1"
 
-source ~/scratch/r3d3_hanabi/bin/activate
-module load libffi
-module load cuda/11.8
-module load OpenSSL/1.1
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate r3d3_hanabi
 
-python r2d2_main.py --config configs/r3d2.yaml --update_freq_text_enc $FREQ --seed $SEED --lm_weights $LM_WEIGHT --num_player $PLAYER --wandb $WANDB --num_of_additional_layer $ADDLAYER
+export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
+
+python r3d2_main.py --config configs/r3d2.yaml --update_freq_text_enc $FREQ --seed $SEED --lm_weights $LM_WEIGHT --num_player $PLAYER --wandb $WANDB --num_of_additional_layer $ADDLAYER
